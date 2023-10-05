6dcd338df4a29dfe811dfc445a25bc29b36641fd
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-10-06 00:50:12 +0900

    Move: railsアプリの階層をプロジェクトのルートに変更

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /opt/ty-dev/docker/rails/tmp/pids/server.pid
+rm -f /opt/ty-dev/tmp/pids/server.pid
 
 # Railsサービスが起動する前にデータベースが存在するか確認して、存在しない場合は作成する
 bundle exec rails db:prepare
