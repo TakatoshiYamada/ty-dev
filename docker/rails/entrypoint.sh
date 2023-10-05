989fc84397dfb649d41a95250626785f6fce0352
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-10-05 23:50:44 +0900

    Add: entrypointにserver.pidを削除する処理を追加

--- docker/rails/entrypoint.sh
+++ docker/rails/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /opt/ty-dev/docker/rails/tmp/pids/server.pid
+
 # Railsサービスが起動する前にデータベースが存在するか確認して、存在しない場合は作成する
 bundle exec rails db:prepare
 
