2f43ec354ee06b3acd56d54f652dd147f8da3b74
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-10-08 23:35:44 +0900

    # Add: アカウント新規登録を無効化

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Defines the articles resource routes
+  # Add: アカウント新規登録を無効化
+  devise_for :users, skip: [:registrations]
 end
