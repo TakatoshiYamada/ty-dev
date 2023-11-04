11618d63605cf343c97b0bf1ea34a35915d73c05
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-11-04 15:37:56 +0900

    Add: Blogの管理機能のControllerを追加

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -1,16 +1,57 @@
+# app/controllers/admin/blogs_controller.rb
 class Admin::BlogsController < ApplicationController
-  def new
+  before_action :authenticate_user!
+  before_action :check_admin
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @blogs = Blog.all
   end
 
-  def create
+  def show
+  end
+
+  def new
+    @blog = Blog.new
   end
 
   def edit
   end
 
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to [:admin, @blog], notice: 'Blog was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def update
+    if @blog.update(blog_params)
+      redirect_to [:admin, @blog], notice: 'Blog was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @blog.destroy
+    redirect_to admin_blogs_url, notice: 'Blog was successfully destroyed.'
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+  def check_admin
+    redirect_to(root_url) unless current_user.admin?
   end
 end
+1
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_085753) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_03_114941) do
   create_table "admin_dashboards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_085753) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
