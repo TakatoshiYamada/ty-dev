54bd36304a7942c49445e3771fa40488cc824328
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-11-11 00:31:54 +0900

    Fix: 単一のDBを扱うようにabstract_classを修正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  #　単一のDBを扱う場合
+  self.abstract_class = true
+
+  # self.abstract_class = trueを内包している
+  # 複数DBを扱う場合
+  # primary_abstract_class
 end
