54aaec9ceea0a8703e92bdc8b9168d9eb1ea6f5a
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-11-18 21:45:26 +0900

    Fix: リダイレクト処理にメッセージ追加

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,6 @@ class AdminController < ApplicationController
   private
 
   def check_admin
-    redirect_to(root_url) unless current_user.admin?
+    redirect_to root_path, alert: 'You are not authorized to view this page.' unless current_user.admin?
   end
 end
