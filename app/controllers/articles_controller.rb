b309b27875e51512668f8eef80742f1ae9877b47
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-11-19 19:38:49 +0900

    Fix: 詳細画面の表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show]
+  # 逆にするとエラーになる
+  before_action :set_blog, only: [:show]
 
   def show
   end
 
   private
+
+  def set_blog
+    @blog = @article.blog
+  end
 
   def set_article
     @article = Article.find(params[:id])
