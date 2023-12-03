7b71866b351302345edaea624dda558eebf8e60c
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2023-12-03 22:10:20 +0900

    Fix: tailswind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+/* opt/ty-dev/tailwind.config.js */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/views/**/*.{html.erb, html.slim, html}",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
