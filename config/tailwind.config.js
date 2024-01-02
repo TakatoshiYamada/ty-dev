f26ba2b9b4e566edcfbaf2ac0ed4f9edbb577b8a
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2024-01-02 23:12:20 +0900

    Fix: erbとslimが明示的になるよう修正

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,9 +3,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './public/*.html',
+    './app/views/**/*.html.erb',
+    './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/javascript/**/*.js'
   ],
   theme: {
     extend: {
