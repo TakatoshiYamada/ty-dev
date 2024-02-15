85e7502c81dccc1326b474e3114ece64e557f16d
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2024-02-16 01:20:18 +0900

    Fix: Site作成seedを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# ユーザーとサイトを作成
 user = User.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: true, name: 'admin')
+site = user.create_site(user_id: 1, name: 'Takatoshi Yamada Arcives', description: 'This is my Site')
 
-# サイト情報を作成
-user.site.create(user_id: 1, name: 'Takatoshi Yamada Arcives', description: 'This is my Site')
-
-Site = HomePage.find_or_create_by!(id: 1)
-Site.image.attach(io: File.open(Rails.root.join("public/images/top_image.jpg")), filename: "top_image.jpg")
+site.top_image.attach(io: File.open(Rails.root.join("public/images/top_image.jpg")), filename: "top_image.jpg")
