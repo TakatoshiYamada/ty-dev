7b985f47629990c88df4df1861d36188c9715e9b
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2024-02-24 21:35:30 +0900

    Add: Userに経歴を記入するtext型のbioを追加

---
+++ db/migrate/20240224123359_add_profile_info_to_users.rb
@@ -0,0 +1,5 @@
+class AddProfileInfoToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :bio, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_21_145533) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_24_123359) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -107,6 +107,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_21_145533) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.string "name"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
