8db42bca5941d536a2592a596af4881cdfa4c422
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2024-02-27 17:49:10 +0900

    Add: プロフィール画像の画面構成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   # Devise のログインチェックをスキップ
   skip_before_action :authenticate_user!, only: [:show]
+  # 画像のアタッチメント
+  has_one_attached :avatar
 
   def show
     @user = User.last
