7db85722faa7f11822799018871734f296ca30b9
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2024-02-28 23:57:15 +0900

    Fix: コメント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   # Devise のログインチェックをスキップ
   skip_before_action :authenticate_user!, only: [:show]
-  # 画像のアタッチメント
   has_one_attached :avatar
 
   def show
